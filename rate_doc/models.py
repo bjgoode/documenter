7aa08e9d4a47bef1fa226862825ca5da7e2b29de
bjgoode <bjgoode@vt.edu>
2018-06-04 11:48:19 -0400

    change field type to char

--- rate_doc/models.py
+++ rate_doc/models.py
@@ -20,11 +20,11 @@ class Doc(models.Model):
     aboutProductLaunch = models.CharField(blank=True, choices=ARTICLE_CLASSIFICATION_QUESTIONS, verbose_name = "Is this article specifically about a product launch?", max_length= 20)
     aboutProductApproval = models.CharField(blank=True, choices=ARTICLE_CLASSIFICATION_QUESTIONS, verbose_name = "Is this article about an approval for a product?", max_length= 20)
     mentionsProduct = models.CharField(blank=True, choices=PRODUCT_MENTION_CHOICES, verbose_name = "If the article mentions the product by name, where is it mentioned?", max_length= 20)
-    ifProductInBodyWhere = models.TextField(blank = True, verbose_name = "If the product is mentioned in the body, approximately where is it mentioned?", max_length= 100)
-    subjectProduct = models.TextField(blank = True, verbose_name = "If the product is mentioned anywhere, what is its name?", max_length= 100)
-    subjectCompany = models.TextField(blank = True, verbose_name = "What company is the article about, if any?", max_length= 100)
-    otherCompany = models.TextField(blank = True, verbose_name = "What other companies are mentioned in the article?", max_length= 100)
-    otherProduct = models.TextField(blank = True, verbose_name = "What other products are mentioned in the article?", max_length= 100)
+    ifProductInBodyWhere = models.CharField(blank = True, verbose_name = "If the product is mentioned in the body, approximately where is it mentioned?", max_length= 200)
+    subjectProduct = models.CharField(blank = True, verbose_name = "If the product is mentioned anywhere, what is its name?", max_length= 200)
+    subjectCompany = models.CharField(blank = True, verbose_name = "What company is the article about, if any?", max_length= 200)
+    otherCompany = models.CharField(blank = True, verbose_name = "What other companies are mentioned in the article?", max_length= 200)
+    otherProduct = models.CharField(blank = True, verbose_name = "What other products are mentioned in the article?", max_length= 200)
 
     # Other Shit
     article_title = models.TextField()
